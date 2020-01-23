49da01c4c45bdcc043ad461591c56ad7ec41f3ae
TanguyCavagna <tanguy.cvgn@eduge.ch>
2020-01-23 15:19:25 +0100

    Ajout de css, fonts et pages

---
+++ includes/nav.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand" href="./">
+        <svg width="100%" height="100%" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M23 0C10.2975 0 0 10.2975 0 23V233C0 245.703 10.2975 256 23 256H233C245.703 256 256 245.703 256 233V23C256 10.2975 245.703 0 233 0H23ZM165.669 96.6C157.669 92.0667 148.735 89.8 138.869 89.8C126.069 89.8 116.002 93.5333 108.669 101V51.6H70.6687V200H106.869V189.2C113.935 197.6 124.602 201.8 138.869 201.8C148.735 201.8 157.669 199.533 165.669 195C173.802 190.333 180.202 183.733 184.869 175.2C189.669 166.667 192.069 156.8 192.069 145.6C192.069 134.4 189.669 124.6 184.869 116.2C180.202 107.667 173.802 101.133 165.669 96.6ZM147.269 164.8C143.002 169.333 137.535 171.6 130.869 171.6C124.202 171.6 118.735 169.333 114.469 164.8C110.202 160.133 108.069 153.733 108.069 145.6C108.069 137.6 110.202 131.333 114.469 126.8C118.735 122.267 124.202 120 130.869 120C137.535 120 143.002 122.267 147.269 126.8C151.535 131.333 153.669 137.6 153.669 145.6C153.669 153.733 151.535 160.133 147.269 164.8Z" fill="currentColor" />
+        </svg>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" /></svg>
+        </span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <form class="form-inline my-2 my-lg-0 search-bar">
+                    <input class="form-control w-md-100" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z" /></svg>
+                    </button>
+                </form>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="./">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z" /></svg>
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./post.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" /></svg>
+                    Post
+                </a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="user" href="#profile">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" /></svg>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
