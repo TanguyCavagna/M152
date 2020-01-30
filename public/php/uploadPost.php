22f5cbbe9ed1ae1c7883d52fbee6605ac03ac796
TanguyCavagna <tanguy.cvgn@gmail.com>
2020-01-30 20:14:50 +0100

    Ajout des test de taille de fichiers

--- public/php/uploadPost.php
+++ public/php/uploadPost.php
@@ -1,26 +1,59 @@
 <?php
+header('Content-Type: application/json');
+
 require __DIR__ . '/../../vendor/autoload.php';
 
 use \App\Controllers\PostController;
 
 define('TARGET_DIR', 'uploads/');
-define('TOTAL_MAX_SIZE', 70);
-define('SINGLE_MAX_SIZE', 3);
+define('TOTAL_MAX_SIZE', 20000000); // byte
+define('SINGLE_MAX_SIZE', 3000000); // byte
 
 $postController = new PostController();
 
-$files = $_FILES['files'];
+if (count($_FILES) > 0) {
+    $files = $_FILES['files'];
+} else {
+    $files = null;
+}
 $post_body = filter_input(INPUT_POST, 'post-body', FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE);
+$size_error = false;
+$all_files_size = 0;
 
-//* Inutile pour le moment mais à garder si jamais
-//$temp = file_get_contents('../json/articles.json');
-//$current_json_articles = json_decode($temp);
+if ($files !== null) {
+    // Test de la taille maximum par fichier et total
+    for ($i = 0; $i < count($files['tmp_name']); $i++) { 
+        if ($files['size'][$i] > SINGLE_MAX_SIZE || $files['error'][$i] == 1) {
+            $size_error = true;
+        }
+        $all_files_size += $files['size'][$i];
+    }
 
-$postController->Insert($post_body, $files);
+    if ($all_files_size > TOTAL_MAX_SIZE) {
+        $size_error = true;
+    }
 
-//* Inutile pour le moment mais à garder si jamais
-//$final_json_articles = json_encode($current_json_articles);
-// TODO: Uncomment this bellow line to update the articles.json (use for research)
-// file_put_contents('../json/articles.json', $final_json_articles);
+    if ($size_error === true) {
+        http_response_code(413);
+        echo json_encode([
+            'errors' => 'Taille maximum de fichiers excédé.'
+        ]);
+        exit();
+    }
+}
 
-echo '';
+if ($postController->Insert($post_body, $files)) {
+    http_response_code(200);
+    echo json_encode([
+        'errors' => []
+    ]);
+    exit();
+} else {
+    http_response_code(200);
+    echo json_encode([
+        'errors' => [
+            'Une erreur est survenue lors de l\'ajout du post.'
+        ]
+    ]);
+    exit();
+}
