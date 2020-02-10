8e1ad9cac7def71d9a151d691e89fa6d6bbc5636
TanguyCavagna <tanguy.cvgn@eduge.ch>
2020-02-11 00:16:18 +0100

    Formatage des dates dans le model de poste

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -4,11 +4,20 @@ namespace App\Models;
 
 class Post {
 
+    /**
+     * Constructeur pour un poste
+     *
+     * @param integer $id
+     * @param string $comment
+     * @param string $creationDate Format Y-m-d H:i:s
+     * @param string $modificationDate Format Y-m-d H:i:s
+     * @param array|null $medias
+     */
     function __construct(int $id, string $comment, string $creationDate, string $modificationDate, ?array $medias) {
         $this->id = $id;
         $this->comment = $comment;
-        $this->creationDate = $creationDate;
-        $this->modificationDate = $modificationDate;
+        $this->creationDate = date_create_from_format('Y-m-d H:i:s', $creationDate);
+        $this->modificationDate = date_create_from_format('Y-m-d H:i:s', $modificationDate);
         $this->medias = $medias;
     }
 
